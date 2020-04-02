8fecd7fba9344f89a0368926bad6c97853ec776b
Otávio Lima <olima94@gmail.com>
2020-04-01 21:41:43 -0300

    :rocket: finished project

---
+++ frontend/src/routes.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Login from './pages/login';
+import Main from './pages/main';
+
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Route path="/" exact component={Login} />
+            <Route path="/dev/:id" component={Main} />
+
+        </BrowserRouter>
+    );
+}
